f8278173b708c890ccae66083ba5115166ad918e
Alex Lamb <alexlamb104@gmail.com>
2022-04-12 12:36:27 -0600

    Completed sum functionality

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -1,16 +1,21 @@
 import React, {useState} from 'react'
 
 function Sum() {
+  const [num1, setNum1] = useState(0)
+  const [num2, setNum2] = useState(0)
+  const [sum, setSum] = useState(null)
+
+  const add = () => {
+    return setSum(+num1 + +num2)
+  }
     
   return (
-
-
     <div className="puzzleBox sumPB">
         <h4> Sum </h4>
-        <input className="inputLine" type="number"/>
-        <input className="inputLine" type="number"/>
-        <button className="confirmationButton" >Submit</button>
-        <span className="resultsBox"></span>
+        <input className="inputLine" type="number" onChange={(e) => setNum1(+e.target.value)}/>
+        <input className="inputLine" type="number" onChange={(e) => setNum2(+e.target.value)}/>
+        <button className="confirmationButton" onClick={() => add()}>Submit</button>
+        <span className="resultsBox">Sum: {sum}</span>
     </div>
   )
 }
