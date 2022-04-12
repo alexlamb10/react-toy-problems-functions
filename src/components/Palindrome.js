abbf2179a246827edf0ae5dbadb4bd03a290c10d
Alex Lamb <alexlamb104@gmail.com>
2022-04-12 12:28:50 -0600

    Finished palindrome checker

--- src/components/Palindrome.js
+++ src/components/Palindrome.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Palindrome() {
+    const [userInput, setUserInput] = useState('')
+    const [palindrome, setPalindrome] = useState('')
+
+    const checkForPalindrome = (props) => {
+        let stringArray = props.split('')
+        let reverse = stringArray.reverse().join('')
+        
+        let pal = reverse === userInput ? 'True' : 'False'
+        setPalindrome(pal)
+
+    }
+
   return (
     <div>
         <div className='puzzleBox palindromePB'>
             <h4>Palindrome</h4>
-            <input type="text" className='inputLine' />
-            <button className='confirmationButton'>Submit</button>
-            <span className='resultsBox'></span>
+            <input type="text" className='inputLine' onChange={(e) => setUserInput(e.target.value)}/>
+            <button className='confirmationButton' onClick={() => checkForPalindrome(userInput)}>Submit</button>
+            <span className='resultsBox'>Palindrome? {palindrome}</span>
         </div>
     </div>
   )
