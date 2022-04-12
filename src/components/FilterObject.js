35e74eaeb8f172ccb6103d877570489865c5c00b
Alex Lamb <alexlamb104@gmail.com>
2022-04-12 11:46:48 -0600

    Completed Filter Object/String

--- src/components/FilterObject.js
+++ src/components/FilterObject.js
@@ -1,14 +1,25 @@
 import React, {useState} from 'react'
 
 function FilterObject() {
+    const [people, setPeople] = useState( [ { "name": "Jimmy Joe", "title": "Hack0r", "age": 12 },
+     { "name": "Jeremy Schrader", "age": 24, "hairColor": "brown" },
+      { "name": "Carly Armstrong", "title": "CEO" } ])
+    const [userInput, setUserInput] = useState('')
+    const [filteredNames, setFilteredNames] = useState([])
+
+    const filterNames = (props) => {
+        let newNames = people.filter(i => i.hasOwnProperty(props))
+        setFilteredNames(newNames)
+    }
+
   return (
     <div>
         <div className='puzzleBox filterObjectPB'>
             <h4>Filter Object</h4>
-            <span className='puzzleText'></span>
-            <input type="text" className='inputLine'/>
-            <button className='confirmationButton'>Submit</button>
-            <span className='resultsBox filterObjectRB'></span>
+            <span className='puzzleText'>Original: {JSON.stringify(people)}</span>
+            <input type="text" className='inputLine' onChange={(e) => setUserInput(e.target.value)}/>
+            <button className='confirmationButton' onClick={() => filterNames(userInput)}>Submit</button>
+            <span className='resultsBox filterObjectRB'>Filtered Names: {JSON.stringify(filteredNames)}</span>
         </div>
     </div>
   )
--- src/components/FilterString.js
+++ src/components/FilterString.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function FilterString() {
+    const [names, setNames] = useState([ "James", "Jessica", "Melody",
+     "Tyler", "Blake", "Jennifer", "Mark", "Maddy" ])
+    const [userInput, setUserInput] = useState('')
+    const [filteredNames, setFilteredNames] = useState([])
+
+    const filterNames = (props) => {
+        let newList = names.filter(i => i.includes(props))
+        setFilteredNames(newList)
+    }
+
   return (
     <div>
         <div className='puzzleBox filterStringPB'>
             <h4>Filter String</h4>
-            <span className='puzzleText'></span>
-            <input type="text" className='inputLine' />
-            <button className='confirmationButton'>Submit</button>
-            <span className='resultsBox filterStringRB'></span>
+            <span className='puzzleText'>Names: {JSON.stringify(names)}</span>
+            <input type="text" className='inputLine' onChange={(e) => setUserInput(e.target.value)}/>
+            <button className='confirmationButton' onClick={() => filterNames(userInput)}>Submit</button>
+            <span className='resultsBox filterStringRB'>Filtered Names: {JSON.stringify(filteredNames)}</span>
         </div>
     </div>
   )
